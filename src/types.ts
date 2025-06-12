fc2790a6dadc706164f85d4c46b3e63cdbcb1dd5
Edward Cui <37892357+ECuiDev@users.noreply.github.com>
2025-06-12 00:46:33 -0700

    Add explicit property typing

--- src/types.ts
+++ src/types.ts
@@ -17,8 +17,10 @@ export interface RybbitConfig {
 
 export type EventType = "pageview" | "custom_event" | "outbound" | "performance";
 
+export type PropertyValue = string | number | boolean;
+
 export interface TrackProperties {
-  [key: string]: any;
+  [key: string]: PropertyValue | PropertyValue[];
 }
 
 export interface OutboundLinkProperties extends TrackProperties {
